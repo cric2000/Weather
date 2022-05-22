0db6a54653455be65aa595f18ba89dfb3577bd1b
Anmol Lamba <anmollamba30@gmail.com>
2022-05-22 13:20:29 +0530

    viewport for mobile

--- index.js
+++ index.js
@@ -134,3 +134,9 @@ window.addEventListener('load',()=>{
           heading.textContent=error;
       }
 });
+
+$(function() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html, body").css({"width":w,"height":h});
+});
